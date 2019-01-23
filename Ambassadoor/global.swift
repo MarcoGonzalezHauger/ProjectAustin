e60550d36db2017dd3ce44be12e147a0707383b3
MarcoGonzalezHauger <marcogonzalezhauger@gmail.com>
2019-01-22 21:15:57 -0500

    made global view more compact

--- Ambassadoor/global.swift
+++ Ambassadoor/global.swift
@@ -23,59 +23,19 @@ import Foundation
 class CentralVariables {
 	
 	//The offers that are currently in the users inbox.
-	var AvaliableOffers: [Offer] = [] {
-		didSet {
-			EachListener(){
-				if let targetfunction = $0.AvaliableOffersChanged {
-					targetfunction()
-				}
-			}
-		}
-	}
+	var AvaliableOffers: [Offer] = [] { didSet { EachListener(){ if let targetfunction = $0.AvaliableOffersChanged { targetfunction()}}}}
 	
 	//The offers that the user has rejected.
-	var RejectedOffers: [Offer] = [] {
-		didSet {
-			EachListener(){
-				if let targetfunction = $0.RejectedOffersChanged {
-					targetfunction()
-				}
-			}
-		}
-	}
+	var RejectedOffers: [Offer] = [] { didSet { EachListener(){ if let targetfunction = $0.RejectedOffersChanged { targetfunction()}}}}
 	
 	//The offers that the user has accepted.
-	var AcceptedOffers: [Offer] = [] {
-		didSet {
-			EachListener(){
-				if let targetfunction = $0.AcceptedOffersChanged {
-					targetfunction()
-				}
-			}
-		}
-	}
+	var AcceptedOffers: [Offer] = [] { didSet { EachListener(){ if let targetfunction = $0.AcceptedOffersChanged { targetfunction()}}}}
 	
 	//The offers the user has completed.
-	var CompletedOffers: [Offer] = [] {
-		didSet {
-			EachListener(){
-				if let targetfunction = $0.CompletedOffersChanged {
-					targetfunction()
-				}
-			}
-		}
-	}
+	var CompletedOffers: [Offer] = [] {	didSet { EachListener(){ if let targetfunction = $0.CompletedOffersChanged { targetfunction()}}}}
 	
 	//The offers the user has completed.
-	var SocialData: [User] = [] {
-		didSet {
-			EachListener(){
-				if let targetfunction = $0.SocialDataChanged {
-					targetfunction()
-				}
-			}
-		}
-	}
+	var SocialData: [User] = [] { didSet { EachListener(){ if let targetfunction = $0.SocialDataChanged{ targetfunction()}}}}
 	
 	//Every VC that is connected to this global variable.
 	func EachListener(updatefor: (_ Listener: GlobalListener) -> ()) {
