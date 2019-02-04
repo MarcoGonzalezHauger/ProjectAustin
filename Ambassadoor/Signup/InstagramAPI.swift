032f3abbaca86cee9ca6463b616549907bf9b793
MarcoGonzalezHauger <marcogonzalezhauger@gmail.com>
2019-02-04 13:54:52 -0500

    changed instagram redirect to https

--- Ambassadoor/Signup/InstagramAPI.swift
+++ Ambassadoor/Signup/InstagramAPI.swift
@@ -4,15 +4,16 @@
 //
 //  Created by Chris Chomicki on 2/2/19.
 //  Copyright © 2019 Tesseract Freelance, LLC. All rights reserved.
+//  Exclusive property of Tesseract Freelnace, LLC.
 //
 
 import Foundation
 
-struct API{
+struct API {
     static let INSTAGRAM_AUTHURL = "https://api.instagram.com/oauth/authorize/"
     static let INSTAGRAM_CLIENT_ID = "fa083c34de6847ff95db596d75ef1c31"
     static let INSTAGRAM_CLIENTSERCRET = "b81172265e6b417782fcf075e2daf2ff"
-    static let INSTAGRAM_REDIRECT_URI = "http://ambassadoor.co"
+    static let INSTAGRAM_REDIRECT_URI = "https://ambassadoor.co"
     static let INSTAGRAM_ACCESS_TOKEN = ""
     static let INSTAGRAM_SCOPE = "follower_list+public_content" /* add whatever scope you need https://www.instagram.com/developer/authorization/ */
 }
