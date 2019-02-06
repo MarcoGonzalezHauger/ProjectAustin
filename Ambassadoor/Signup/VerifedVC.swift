ea4019b71dfb58136242853925b039e854317d21
MarcoGonzalezHauger <marcogonzalezhauger@gmail.com>
2019-02-05 19:00:36 -0500

    Added varied page.

---
+++ Ambassadoor/Signup/VerifedVC.swift
@@ -0,0 +1,30 @@
+//
+//  VerifedVC.swift
+//  Ambassadoor
+//
+//  Created by Marco Gonzalez Hauger on 2/5/19.
+//  Copyright © 2019 Tesseract Freelance, LLC. All rights reserved.
+//
+
+import UIKit
+
+class VerifedVC: UIViewController {
+
+	var username: String!
+	
+	@IBOutlet weak var usernameLabel: UILabel!
+	
+	override func viewDidLoad() {
+        super.viewDidLoad()
+		
+    }
+	
+	@IBAction func proceed(_ sender: Any) {
+		
+	}
+	
+	@IBAction func notme(_ sender: Any) {
+		
+	}
+	
+}
