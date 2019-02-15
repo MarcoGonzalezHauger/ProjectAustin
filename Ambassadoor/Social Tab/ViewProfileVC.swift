072c2dfdf9980d2d0dcb4887eaf068572b351055
MarcoGonzalezHauger <marcogonzalezhauger@gmail.com>
2019-02-15 16:40:05 -0500

    added social VC

---
+++ Ambassadoor/Social Tab/ViewProfileVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewProfileVC.swift
+//  Ambassadoor
+//
+//  Created by Marco Gonzalez Hauger on 2/14/19.
+//  Copyright © 2019 Tesseract Freelance, LLC. All rights reserved.
+//  Exclusive property of Tesseract Freelance, LLC.
+//
+
+import UIKit
+
+class ViewProfileVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
