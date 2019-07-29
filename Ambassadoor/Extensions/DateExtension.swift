3ffbdd82fe19b560d1ef7c72ceafe5ce7a373a04
K Saravana Kumar <ksaravanakumar@Ks-iMac-NaveeN.local>
2019-07-29 17:34:20 +0530

    extension added

---
+++ Ambassadoor/Extensions/DateExtension.swift
@@ -0,0 +1,30 @@
+//
+//  DateExtension.swift
+//  Ambassadoor
+//
+//  Created by K Saravana Kumar on 25/07/19.
+//  Copyright © 2019 Tesseract Freelance, LLC. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension Date {
+    static var yesterday: Date { return Date().dayBefore }
+    static var tomorrow:  Date { return Date().dayAfter }
+    var dayBefore: Date {
+        return Calendar.current.date(byAdding: .day, value: -1, to: noon)!
+    }
+    var dayAfter: Date {
+        return Calendar.current.date(byAdding: .day, value: 1, to: noon)!
+    }
+    var noon: Date {
+        return Calendar.current.date(bySettingHour: 12, minute: 0, second: 0, of: self)!
+    }
+    var month: Int {
+        return Calendar.current.component(.month,  from: self)
+    }
+    var isLastDayOfMonth: Bool {
+        return dayAfter.month != month
+    }
+}
