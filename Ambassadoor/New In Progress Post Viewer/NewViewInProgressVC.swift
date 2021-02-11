3d9bc84c211aba9523f66b7dc6152a598cf0d6e3
MarcoGonzalezHauger <marcogonzalezhauger@gmail.com>
2021-02-11 11:42:16 -0500

    fixed issues w/ code

--- Ambassadoor/New In Progress Post Viewer/NewViewInProgressVC.swift
+++ Ambassadoor/New In Progress Post Viewer/NewViewInProgressVC.swift
@@ -22,8 +22,11 @@ class NewViewInProgressVC: UIViewController {
 	//Orb View / Time left view.
 	@IBOutlet weak var orbView: ShadowView!
 	@IBOutlet var orbs: [UIView]!
+	@IBOutlet weak var orbStack: UIStackView!
 	@IBOutlet weak var missionLabel: UILabel!
 	@IBOutlet weak var timeLeftLabel: UILabel!
+	let radarView: UIView = UIView()
+	var isDoingAnimations = false
 	
 	//status view
 	@IBOutlet weak var statusView: ShadowView!
@@ -59,6 +62,7 @@ class NewViewInProgressVC: UIViewController {
         super.viewDidLoad()
 		scrollView.alwaysBounceVertical = false
 		orbs.sort{$0.tag > $1.tag}
+		updateConents()
     }
 	
 	@IBAction func closeButtonPressed(_ sender: Any) {
@@ -94,11 +98,21 @@ class NewViewInProgressVC: UIViewController {
 		captionRequirementsView.isHidden = 	!["Accepted"]												.contains(thisInProgressPost.status)
 		youHaveBeenPaidView.isHidden = 		!["Paid"]													.contains(thisInProgressPost.status)
 		cancelPostView.isHidden = 			!["Accepted", "Posted", "Verified"]							.contains(thisInProgressPost.status)
+		
+		updateOrbStatus()
 	}
 	
 }
 
 extension NewViewInProgressVC {
+	
+	func viewToFrame(_ view: UIView) -> CGRect {
+		var newView: CGRect!
+		newView = orbStack.convert(view.frame, to: self.view)
+		
+		return newView
+	}
+	
 	func updateOrbStatus() {
 		switch thisInProgressPost.status {
 		
@@ -109,46 +123,46 @@ extension NewViewInProgressVC {
 			timeLeftLabel.text = ""
 			moneyView.backgroundColor = .systemGreen
 			setTimeLeftToPost()
-			setRadar(toRect: viewToFrame(acceptedOrb), color: .systemGreen)
+			setRadar(toRect: viewToFrame(orbs[0]), color: .systemGreen)
 			
 		case "Posted":
 			makeViewColorUpTo(upToIndex: 4, filledColor: .systemBlue, otherColor: .systemGray)
-			currentMission.text = "If post is verified, you will be paid in"
+			missionLabel.text = "If post is verified, you will be paid in"
 			moneyView.backgroundColor = .systemGreen
 			setPaidInLabel()
-			setRadar(toRect: viewToFrame(PostedOrb), color: .systemBlue)
+			setRadar(toRect: viewToFrame(orbs[4]), color: .systemBlue)
 			
 		case "Verified":
 			makeViewColorUpTo(upToIndex: 8, filledColor: .systemGreen, otherColor: .systemGray)
-			currentMission.text = "You will be paid in"
+			missionLabel.text = "You will be paid in"
 			moneyView.backgroundColor = .systemGreen
 			setPaidInLabel()
-			setRadar(toRect: viewToFrame(verifiedOrb), color: .systemGreen)
+			setRadar(toRect: viewToFrame(orbs[8]), color: .systemGreen)
 			
 		case "Paid":
 			makeViewColorUpTo(upToIndex: 12, filledColor: .systemYellow, otherColor: .systemGray)
-			currentMission.text = "Withdraw in the Profile tab."
+			missionLabel.text = "Withdraw in the Profile tab."
 			timeLeftLabel.text = "You have been Paid"
 			moneyView.backgroundColor = .systemGreen
 			removeRadar()
 			
 		case "Rejected":
 			makeViewColorUpTo(upToIndex: 8, filledColor: .systemRed, otherColor: .systemGray)
-			currentMission.text = "Click to see why."
+			missionLabel.text = "Click to see why."
 			timeLeftLabel.text = "Post Rejected"
 			moneyView.backgroundColor = .systemRed
 			removeRadar()
 			
 		case "Cancelled":
 			makeViewColorUpTo(upToIndex: 12, filledColor: .systemRed, otherColor: .systemGray)
-			currentMission.text = "You cancelled this post."
+			missionLabel.text = "You cancelled this post."
 			timeLeftLabel.text = "Cancelled"
 			moneyView.backgroundColor = .systemRed
 			removeRadar()
 			
 		case "Expired":
 			makeViewColorUpTo(upToIndex: 4, filledColor: .systemRed, otherColor: .systemGray)
-			currentMission.text = "You didn't post in time."
+			missionLabel.text = "You didn't post in time."
 			timeLeftLabel.text = "Post Expired"
 			moneyView.backgroundColor = .systemRed
 			removeRadar()
@@ -178,8 +192,8 @@ extension NewViewInProgressVC {
 	
 	func setRadar(toRect: CGRect, color: UIColor) {
 		if !isDoingAnimations {
-			self.addSubview(radarView)
-			self.bringSubviewToFront(radarView)
+			view.addSubview(radarView)
+			view.bringSubviewToFront(radarView)
 		}
 		radarView.frame = toRect
 		radarView.backgroundColor = color
@@ -191,7 +205,6 @@ extension NewViewInProgressVC {
 		}
 	}
 	
-	var isDoingAnimations = false
 	
 	func doRadarAnimation() {
 		let animation = CAKeyframeAnimation.init(keyPath: "transform.scale")
@@ -221,11 +234,11 @@ extension NewViewInProgressVC {
 	
 	func makeViewColorUpTo(upToIndex: Int, filledColor: UIColor, otherColor: UIColor) {
 		for i in 0...upToIndex {
-			orbView[i].backgroundColor = filledColor
+			orbs[i].backgroundColor = filledColor
 		}
-		if upToIndex < orbView.count - 1 {
-			for i in (upToIndex + 1)...(orbView.count - 1) {
-				orbView[i].backgroundColor = otherColor
+		if upToIndex < orbs.count - 1 {
+			for i in (upToIndex + 1)...(orbs.count - 1) {
+				orbs[i].backgroundColor = otherColor
 			}
 		}
 	}
