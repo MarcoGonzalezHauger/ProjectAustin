e67edcc18d2cc6ec9a02c7a0ca48acbdb45d5336
Ramkumar <ramkumar@securenext.in>
2021-07-28 19:44:15 +0530

    New Welcom classes added and business link created

---
+++ Ambassadoor/Intro Setup/NewWelcomeVC.swift
@@ -0,0 +1,33 @@
+//
+//  NewWelcomeVC.swift
+//  Ambassadoor
+//
+//  Created by K Saravana Kumar on 28/07/21.
+//  Copyright © 2021 Tesseract Freelance, LLC. All rights reserved.
+//
+
+import UIKit
+
+class NewWelcomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func openAppStore(sender: UIButton){
+        OpenAppStoreID(id: "amassadoorbusiness/id1483207154")
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
